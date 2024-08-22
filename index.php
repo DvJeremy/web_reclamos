7208d921c8c903d802e560ae31ead549d3ff8beb
jeremyUnfv <jeremy.amazing.it@gmail.com>
2024-08-22 18:29:42 -0500

    union barra de nav

--- index.php
+++ index.php
@@ -4,12 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-<?php include 'layouts/navbar.php'; ?>
-<h1>hola</h1>
-<h2>hola</h2>
-<h3>hola</h3>
-<?php include 'layouts/footer.php'; ?>
+    <?php include 'layouts/navbar.php'; ?>
+    
+    <div class="container mt-5">
+        <div id="dynamic-content">
+            <!-- Aquí se cargará el contenido dinámico -->
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            // Cargar contenido inicial
+            $("#dynamic-content").load("../Reclamos/libro_reclamaciones.php");
+
+            // Evento para manejar los clics en los enlaces de la navbar
+            $(".nav-link").click(function(e){
+                e.preventDefault(); // Prevenir la recarga de la página
+                var targetPage = $(this).data('target');
+                $("#dynamic-content").load("../" + targetPage);
+            });
+        });
+    </script>
+    <?php include 'layouts/footer.php'; ?>
 </body>
 </html>
